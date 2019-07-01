67eb26dbb7e35b5c9e36f81ef5d2705826734de1
Esteban Freyre <esteban.freyre@algosky.net>
2019-07-01 09:02:45 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
-#' Dashboard app for mtcars dataset analisys
-#' @param mpg and mpgsp as predictors
-#' @param hp representing the response
+#' A user interface for interaction with model parameters and slider or tab options
+#'
+#' @param slider input data for responsive dashboard
 #' @return a 'lm' object representing the linear model with the corresponding prediction for hp
 #' @return a 'plot' object representing the two predictions for the different models
 #' @return a 'plot' object highlighting the car model selected
@@ -13,50 +13,49 @@
 #' One for model prediction comparisons and the other one for interactive table.
 #' The second tab allows choosing the number of rows shown and there is a search space for filtering cars' models which
 #' will be highlighted in the graph window.
-#' @see also \code{lm}
-#' @import mtcars, shiny, shinydashboard, DT, stats
+#' @seealso \code{lm}
 #' @export
 
 library(shiny)
 library(shinydashboard)
-data(mtcars)
 library(DT)
+data(mtcars)
 
 header <- dashboardHeader(title ="DDP - Cars Proyect")
 
 ## Sidebar content
 sidebar<-dashboardSidebar(
-    sidebarMenu(
-        menuItem("Dashboard", tabName = "Dashboard", icon = icon("dashboard")),
-        menuItem("Table", tabName = "Table", icon = icon("th")),
-        sliderInput("sliderMPG", "Select the MPG of the car?", 10, 35, value = 20),
-        checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
-        checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
-    )
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "Dashboard", icon = icon("dashboard")),
+    menuItem("Table", tabName = "Table", icon = icon("th")),
+    sliderInput("sliderMPG", "Select the MPG of the car?", 10, 35, value = 20),
+    checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
+    checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
+  )
 )
 
 ## Body content
 body<-dashboardBody(
-    tabItems(
-        # First tab content
-        tabItem(tabName = "Dashboard",
-                fluidRow(
-                    column(width = 11,
-                           box(title = "HP vs MPG Models Forecasts",width = NULL, status = "primary",
-                               plotOutput("plot1"),collapsible = TRUE)))
-        ),
+  tabItems(
+    # First tab content
+    tabItem(tabName = "Dashboard",
+            fluidRow(
+              column(width = 11,
+                     box(title = "HP vs MPG Models Forecasts",width = NULL, status = "primary",
+                         plotOutput("plot1"),collapsible = TRUE)))
+    ),
 
-        # Second tab content
-        tabItem(tabName = "Table",
-                fluidPage(
-                    title = 'Data Table',
-                    h3('Enter Filter Criteria'),
-                    fluidRow(
-                        column(6, DT::dataTableOutput('x1')),
-                        column(6, plotOutput('x2', height = 500))
-                    )
-                )
-        )
-    ))
+    # Second tab content
+    tabItem(tabName = "Table",
+            fluidPage(
+              title = 'Data Table',
+              h3('Enter Filter Criteria'),
+              fluidRow(
+                column(6, DT::dataTableOutput('x1')),
+                column(6, plotOutput('x2', height = 500))
+              )
+            )
+    )
+  ))
 
 ui<-dashboardPage(header, sidebar, body)
